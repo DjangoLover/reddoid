6f68fa1e45ea1a21fa76d8135d295005b0fb3082
Oleksandr Polyeno <polyenoom@gmail.com>
2013-09-28 21:47:28 +0300

    used pure.css

--- reddoid/reddoid/settings/base.py
+++ reddoid/reddoid/settings/base.py
@@ -1,8 +1,5 @@
-from os.path import abspath, basename, dirname, join, normpath
+from .utils import rel
 # Django settings for reddoid project.
-
-DJANGO_ROOT = dirname(dirname(abspath(__file__)))
-SITE_ROOT = dirname(DJANGO_ROOT)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -54,18 +51,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = rel('media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = rel('media/static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -73,6 +70,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    rel('static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -115,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    normpath(join(SITE_ROOT, 'templates')),
+    rel('templates'),
 )
 
 INSTALLED_APPS = (
@@ -165,10 +163,10 @@ LOGGING = {
 
 # python social auth
 AUTHENTICATION_BACKENDS = (
-      'social.backends.google.GoogleOAuth',
-      'social.backends.facebook.FacebookOAuth2',
-      'social.backends.twitter.TwitterOAuth',
-      'django.contrib.auth.backends.ModelBackend',
+    'social.backends.google.GoogleOAuth',
+    'social.backends.facebook.FacebookOAuth2',
+    'social.backends.twitter.TwitterOAuth',
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
