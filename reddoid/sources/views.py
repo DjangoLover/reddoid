54cf9dfe00f2f6ed10045e0efc73120fa71f0310
Eugene Lisitsky <lisitsky@gmail.com>
2013-09-30 02:48:44 +0400

    Better look for sources page. Order by number of posts

--- reddoid/sources/views.py
+++ reddoid/sources/views.py
@@ -8,9 +8,13 @@ from django.views.decorators import csrf
 from django.views.generic import ListView
 from django.views.generic.base import TemplateView, View
 from django.utils import timezone, simplejson
+from django.db.models import Count, Sum
 
 from models import Source
 
 class SourcesView(ListView):
-	# template_name = 'list.html'
-	model = Source
+    # template_name = 'list.html'
+    model = Source
+
+    def get_queryset(self):
+        return Source.objects.annotate(num=Count('post')).order_by('-num')
